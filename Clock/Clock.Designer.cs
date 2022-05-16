4f819e84f48c1806662b6dbc913a4bb8c729b47b
Just-Person <ilja.guryanow3636@yandex.ru>
2022-05-16 23:17:21 +0300

    Add files

---
+++ Clock/Clock.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace Clock
+{
+    partial class Clock
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.h1 = new System.Windows.Forms.Label();
+            this.h2 = new System.Windows.Forms.Label();
+            this.h3 = new System.Windows.Forms.Label();
+            this.h4 = new System.Windows.Forms.Label();
+            this.h5 = new System.Windows.Forms.Label();
+            this.h6 = new System.Windows.Forms.Label();
+            this.h7 = new System.Windows.Forms.Label();
+            this.h8 = new System.Windows.Forms.Label();
+            this.h9 = new System.Windows.Forms.Label();
+            this.h10 = new System.Windows.Forms.Label();
+            this.h11 = new System.Windows.Forms.Label();
+            this.h12 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // h1
+            // 
+            this.h1.AutoSize = true;
+            this.h1.BackColor = System.Drawing.Color.Transparent;
+            this.h1.ForeColor = System.Drawing.Color.Purple;
+            this.h1.Location = new System.Drawing.Point(323, 174);
+            this.h1.Name = "h1";
+            this.h1.Size = new System.Drawing.Size(10, 16);
+            this.h1.TabIndex = 0;
+            this.h1.Text = "I";
+            // 
+            // h2
+            // 
+            this.h2.AutoSize = true;
+            this.h2.ForeColor = System.Drawing.Color.Purple;
+            this.h2.Location = new System.Drawing.Point(350, 203);
+            this.h2.Name = "h2";
+            this.h2.Size = new System.Drawing.Size(13, 16);
+            this.h2.TabIndex = 0;
+            this.h2.Text = "II";
+            // 
+            // h3
+            // 
+            this.h3.AutoSize = true;
+            this.h3.BackColor = System.Drawing.Color.Transparent;
+            this.h3.ForeColor = System.Drawing.Color.Purple;
+            this.h3.Location = new System.Drawing.Point(361, 244);
+            this.h3.Name = "h3";
+            this.h3.Size = new System.Drawing.Size(16, 16);
+            this.h3.TabIndex = 0;
+            this.h3.Text = "III";
+            // 
+            // h4
+            // 
+            this.h4.AutoSize = true;
+            this.h4.BackColor = System.Drawing.Color.Transparent;
+            this.h4.ForeColor = System.Drawing.Color.Purple;
+            this.h4.Location = new System.Drawing.Point(346, 285);
+            this.h4.Name = "h4";
+            this.h4.Size = new System.Drawing.Size(19, 16);
+            this.h4.TabIndex = 0;
+            this.h4.Text = "IV";
+            // 
+            // h5
+            // 
+            this.h5.AutoSize = true;
+            this.h5.BackColor = System.Drawing.Color.Transparent;
+            this.h5.ForeColor = System.Drawing.Color.Purple;
+            this.h5.Location = new System.Drawing.Point(320, 310);
+            this.h5.Name = "h5";
+            this.h5.Size = new System.Drawing.Size(16, 16);
+            this.h5.TabIndex = 0;
+            this.h5.Text = "V";
+            // 
+            // h6
+            // 
+            this.h6.AutoSize = true;
+            this.h6.BackColor = System.Drawing.Color.Transparent;
+            this.h6.ForeColor = System.Drawing.Color.Purple;
+            this.h6.Location = new System.Drawing.Point(274, 315);
+            this.h6.Name = "h6";
+            this.h6.Size = new System.Drawing.Size(19, 16);
+            this.h6.TabIndex = 0;
+            this.h6.Text = "VI";
+            // 
+            // h7
+            // 
+            this.h7.AutoSize = true;
+            this.h7.BackColor = System.Drawing.Color.Transparent;
+            this.h7.ForeColor = System.Drawing.Color.Purple;
+            this.h7.Location = new System.Drawing.Point(230, 310);
+            this.h7.Name = "h7";
+            this.h7.Size = new System.Drawing.Size(22, 16);
+            this.h7.TabIndex = 0;
+            this.h7.Text = "VII";
+            // 
+            // h8
+            // 
+            this.h8.AutoSize = true;
+            this.h8.BackColor = System.Drawing.Color.Transparent;
+            this.h8.ForeColor = System.Drawing.Color.Purple;
+            this.h8.Location = new System.Drawing.Point(205, 285);
+            this.h8.Name = "h8";
+            this.h8.Size = new System.Drawing.Size(25, 16);
+            this.h8.TabIndex = 0;
+            this.h8.Text = "VIII";
+            // 
+            // h9
+            // 
+            this.h9.AutoSize = true;
+            this.h9.BackColor = System.Drawing.Color.Transparent;
+            this.h9.ForeColor = System.Drawing.Color.Purple;
+            this.h9.Location = new System.Drawing.Point(190, 244);
+            this.h9.Name = "h9";
+            this.h9.Size = new System.Drawing.Size(18, 16);
+            this.h9.TabIndex = 0;
+            this.h9.Text = "IX";
+            // 
+            // h10
+            // 
+            this.h10.AutoSize = true;
+            this.h10.BackColor = System.Drawing.Color.Transparent;
+            this.h10.ForeColor = System.Drawing.Color.Purple;
+            this.h10.Location = new System.Drawing.Point(203, 201);
+            this.h10.Name = "h10";
+            this.h10.Size = new System.Drawing.Size(15, 16);
+            this.h10.TabIndex = 0;
+            this.h10.Text = "X";
+            // 
+            // h11
+            // 
+            this.h11.AutoSize = true;
+            this.h11.BackColor = System.Drawing.Color.Transparent;
+            this.h11.ForeColor = System.Drawing.Color.Purple;
+            this.h11.Location = new System.Drawing.Point(235, 174);
+            this.h11.Name = "h11";
+            this.h11.Size = new System.Drawing.Size(18, 16);
+            this.h11.TabIndex = 0;
+            this.h11.Text = "XI";
+            // 
+            // h12
+            // 
+            this.h12.AutoSize = true;
+            this.h12.BackColor = System.Drawing.Color.Transparent;
+            this.h12.ForeColor = System.Drawing.Color.Purple;
+            this.h12.Location = new System.Drawing.Point(272, 164);
+            this.h12.Name = "h12";
+            this.h12.Size = new System.Drawing.Size(21, 16);
+            this.h12.TabIndex = 0;
+            this.h12.Text = "XII";
+            // 
+            // Clock
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(556, 460);
+            this.Controls.Add(this.h1);
+            this.Controls.Add(this.h2);
+            this.Controls.Add(this.h3);
+            this.Controls.Add(this.h4);
+            this.Controls.Add(this.h5);
+            this.Controls.Add(this.h6);
+            this.Controls.Add(this.h7);
+            this.Controls.Add(this.h8);
+            this.Controls.Add(this.h9);
+            this.Controls.Add(this.h10);
+            this.Controls.Add(this.h11);
+            this.Controls.Add(this.h12);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Clock";
+            this.Text = "Часы";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Label h1;
+        private System.Windows.Forms.Label h2;
+        private System.Windows.Forms.Label h3;
+        private System.Windows.Forms.Label h4;
+        private System.Windows.Forms.Label h5;
+        private System.Windows.Forms.Label h6;
+        private System.Windows.Forms.Label h7;
+        private System.Windows.Forms.Label h8;
+        private System.Windows.Forms.Label h9;
+        private System.Windows.Forms.Label h10;
+        private System.Windows.Forms.Label h11;
+        private System.Windows.Forms.Label h12;
+    }
+}
---
+++ Clock/Clock.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing.Drawing2D;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Clock
+{
+    public partial class Clock : Form
+    {
+        public Clock()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer.Enabled = true;
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            Invalidate();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics gr = e.Graphics;
+            GraphicsState grSt;
+
+            int width = this.Width;
+            int height = this.Height;
+
+            DateTime dateTime = DateTime.Now;
+            gr.TranslateTransform(width / 2, height / 2);
+
+            Pen penGreen = new Pen(Color.Blue, 1);
+            Pen penWhite = new Pen(Color.Green, 3);
+            Pen penYellow = new Pen(Color.HotPink, 5);
+            Pen penRed = new Pen(Color.Red, 3);
+
+
+            gr.DrawEllipse(penWhite, -100, -100, 200, 200);
+
+            grSt = gr.Save();
+            gr.RotateTransform(6 * dateTime.Second);
+            gr.DrawLine(penGreen, -3, 0, -65, -65);
+            gr.DrawLine(penGreen, 3, 0, -65, -65);
+            gr.Restore(grSt);
+
+            grSt = gr.Save();
+            gr.RotateTransform(6 * dateTime.Minute + dateTime.Second / 5);
+            gr.DrawLine(penRed, 0, 0, -55, -55);
+            gr.Restore(grSt);
+
+            grSt = gr.Save();
+            gr.RotateTransform(6 * dateTime.Hour + dateTime.Minute / 5);
+            gr.DrawLine(penYellow, 0, 0, -43, -43);
+            gr.DrawLine(penYellow, -40, -40, -40, -30);
+            gr.DrawLine(penYellow, -40, -40, -30, -40);
+            gr.Restore(grSt);
+
+            for (int i = 0; i < 12; ++i)
+            {
+                grSt = gr.Save();
+                gr.RotateTransform(30 * i + 45);
+                gr.DrawLine(penWhite, -50, -50, -70, -70);
+                gr.Restore(grSt);
+            }
+
+            for (int i = 0; i < 60; ++i)
+            {
+                grSt = gr.Save();
+                gr.RotateTransform(6 * i);
+                gr.DrawLine(penWhite, -63, -63, -70, -70);
+                gr.Restore(grSt);
+            }
+
+        }
+    }
+}
